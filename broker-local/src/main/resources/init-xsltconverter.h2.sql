5e345a69d0a29c9339692aac441203b6e0c950e2
ArneBinder <arne.b.binder@gmail.com>
2016-07-29 15:42:55 +0200

    fixed init-xsltconverter.h2.sql #16

--- broker-local/src/main/resources/init-xsltconverter.h2.sql
+++ broker-local/src/main/resources/init-xsltconverter.h2.sql
@@ -35,7 +35,7 @@ VALUES
             </xsl:for-each>
         </xsl:variable>
         <span xmlns="http://www.w3.org/1999/xhtml" title="{$attributeValuePairs}"
-            class="{concat(\''convert-\'',name())}">
+            class="{concat(''convert-'',name())}">
             <xsl:apply-templates mode="renameElem"/>
         </span>
     </xsl:template>
@@ -59,7 +59,7 @@ VALUES
     <xsl:variable name="input"> &lt;its:locQualityIssues xml:id="lq1" xmlns:its="http://www.w3.org/2005/11/its"&gt;
         &lt;its:locQualityIssue
         locQualityIssueType="misspelling"
-        locQualityIssueComment="\''c\''es\'' is unknown. Could be \''c\''est\''"
+        locQualityIssueComment="''c''es'' is unknown. Could be ''c''est''"
         locQualityIssueSeverity="50"/&gt;
         &lt;its:locQualityIssue
         locQualityIssueType="typographical"
@@ -74,11 +74,14 @@ VALUES
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
     xmlns:xs="http://www.w3.org/2001/XMLSchema" exclude-result-prefixes="xlf xs"
     xmlns:xlf="urn:oasis:names:tc:xliff:document:2.0" version="1.0" xmlns="http://www.w3.org/1999/xhtml">
-    <xsl:output method="html" indent="yes"/>
+    <xsl:output method="xml" indent="yes" omit-xml-declaration="yes"/>
     <xsl:template match="/">
         <html>
             <head>
-                <title>dummy title</title>
+                <title>@@@</title>
+                <script type="application/xml">
+                    <xsl:apply-templates select="/" mode="copyFragment"/>
+                </script>
             </head>
             <body>
                 <div id="xyz1xyz">
@@ -91,6 +94,15 @@ VALUES
                 </div>
             </body>
         </html>
+    </xsl:template>
+    <xsl:template mode="copyFragment" match="node()|@*">
+        <xsl:copy>
+            <xsl:apply-templates select="node()|@*" mode="copyFragment"/>
+        </xsl:copy>
+    </xsl:template>
+    <xsl:template mode="copyFragment" match="xlf:source">
+        <xsl:variable name="number">n<xsl:number level="any"/></xsl:variable>
+        <anchor id="{$number}"/>
     </xsl:template>
     <xsl:template mode="renameElem" match="*">
         <xsl:variable name="attributeValuePairs">
@@ -105,7 +117,7 @@ VALUES
             </xsl:for-each>
         </xsl:variable>
         <span xmlns="http://www.w3.org/1999/xhtml" title="{$attributeValuePairs}"
-            class="{concat(\''convert - \'', name())}">
+            class="{concat(''convert-'',name())}">
             <xsl:apply-templates mode="renameElem"/>
         </span>
     </xsl:template>
@@ -116,14 +128,14 @@ VALUES
         <xsl:value-of select="$myparam"/>
     </xsl:template>
 </xsl:stylesheet>','admin'),
-(0,'Conversion of HTML to XLIFF 2.0. The stylesheet currently processes only e-Entity output.',1,'html-to-xliff20','<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:itsm="urn:oasis:names:tc:xliff:itsm:2.1"
-    xmlns:xs="http://www.w3.org/2001/XMLSchema" exclude-result-prefixes="xs h xlf fn"
-    xmlns:xlf="urn:oasis:names:tc:xliff:document:2.0" version="3.0" xmlns:h="http://www.w3.org/1999/xhtml"
-    xmlns:fn="http://www.w3.org/2005/xpath-functions"
-    xmlns="urn:oasis:names:tc:xliff:document:2.0" >
-    <xsl:output method="xml"/>
+(0,'Conversion of HTML to XLIFF 2.0. The stylesheet currently processes only e-Entity output.',1,'html-to-xliff20','<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:itsm="urn:oasis:names:tc:xliff:itsm:2.1" xmlns:xs="http://www.w3.org/2001/XMLSchema"
+    exclude-result-prefixes="xs h xlf fn" xmlns:xlf="urn:oasis:names:tc:xliff:document:2.0"
+    version="3.0" xmlns:h="http://www.w3.org/1999/xhtml"
+    xmlns:fn="http://www.w3.org/2005/xpath-functions" xmlns="urn:oasis:names:tc:xliff:document:2.0">
+    <xsl:output method="xml" omit-xml-declaration="yes"/>
     <xsl:variable name="originalInputDoc">
-        <xsl:copy-of select="fn:parse-xml(//*[local-name()=\''script\''])"></xsl:copy-of>
+        <xsl:copy-of select="fn:parse-xml(//*[local-name()=''script''])"/>
     </xsl:variable>
     <xsl:variable name="htmlDoc">
         <xsl:copy-of select="/*"/>
@@ -133,19 +145,19 @@ VALUES
     </xsl:template>
     <xsl:template match="node() | @*" mode="copy">
         <xsl:copy>
-            <xsl:apply-templates select="node()| @*" mode="copy"/>
+            <xsl:apply-templates select="node() | @*" mode="copy"/>
         </xsl:copy>
     </xsl:template>
     <xsl:template match="h:anchor" mode="copy">
         <source>
             <xsl:variable name="position">n<xsl:number level="any"/></xsl:variable>
             <xsl:for-each select="$htmlDoc">
-                <xsl:apply-templates select="//*[@id=$position]/node()" mode="writeAnnotation"/>
+                <xsl:apply-templates select="//*[@id = $position]/node()" mode="writeAnnotation"/>
             </xsl:for-each>
         </source>
     </xsl:template>
-    <xsl:template match="span[@class[starts-with(., \''convert-\'')]]" mode="writeAnnotation">
-        <xsl:element name="{substring-after(@class,\''convert-\'')}">
+    <xsl:template match="span[@class[starts-with(., ''convert-'')]]" mode="writeAnnotation">
+        <xsl:element name="{substring-after(@class,''convert-'')}">
             <xsl:if test="string-length(@title) > 0">
                 <xsl:call-template name="writeAttrs">
                     <xsl:with-param name="attList" select="@title"/>
@@ -154,7 +166,7 @@ VALUES
             <xsl:apply-templates mode="writeAnnotation"/>
         </xsl:element>
     </xsl:template>
-    <xsl:template mode="writeAnnotation" match="span[@its-ta-ident-ref]">
+    <xsl:template mode="writeAnnotation" match="*[local-name()=''span'' and @its-ta-ident-ref]">
         <mrk id="{generate-id()}" type="itsm:generic">
             <xsl:attribute name="itsm:taIdentRef">
                 <xsl:value-of select="@its-ta-ident-ref"/>
@@ -165,16 +177,16 @@ VALUES
     <xsl:template name="writeAttrs">
         <xsl:param name="attList"/>
         <xsl:variable name="name"
-            select="substring-before(substring-after($attList, \''@@@delim@@@name:\''), \''@@@value:\'')"/>
+            select="substring-before(substring-after($attList, ''@@@delim@@@name:''), ''@@@value:'')"/>
         <xsl:variable name="value"
-            select="substring-before(substring-after($attList, \''@@@value:\''), \''@@@delim@@@\'')"/>
+            select="substring-before(substring-after($attList, ''@@@value:''), ''@@@delim@@@'')"/>
         <xsl:if test="($name)">
             <xsl:attribute name="{$name}">
                 <xsl:value-of select="$value"/>
             </xsl:attribute>
             <xsl:variable name="rest">
                 <xsl:value-of
-                    select="substring($attList, string-length(concat(\''@@@delim@@@name:\'', $name, \''@@@value:\'', $value)) + 1)"
+                    select="substring($attList, string-length(concat(''@@@delim@@@name:'', $name, ''@@@value:'', $value)) + 1)"
                 />
             </xsl:variable>
             <xsl:if test="$rest">
